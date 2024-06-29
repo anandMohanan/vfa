376fb6ba223c3f8dca88a86f350c68829f89c9e8
Anand Mohanan <anandmohanan@voicefirstai.com>
2024-06-29 12:38:06 +0530

    some submit page fix

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -23,14 +23,10 @@ const SubmitPage = () => {
                     <h1 className={cn("text-3xl  md:text-5xl text-center font-extrabold",
                         "  text-transparent !bg-clip-text [background:linear-gradient(92.46deg,_#698aff,_#3f5399)]"
                         , primary_font.className)}>See and hear the technology first-hand </h1>
-                    <Image src="/activate.png" alt="activate" width={400} height={100} className="w-full h-[80%]" priority />
-                </div>
-                <div className="flex grow lg:h-[50vh]">
-                    <ul className={cn("list-disc p-2 ml-6 lg:text-xl text-lg  text-white", primary_font.className)}>
-                        <li>Discuss integration, support, and customization</li>
-                        <li>Compare VoiceFIrst AI to other voice platforms</li>
-                        <li>Discuss pricing and timeline </li>
-                    </ul>
+                    <Image src="/activate.png" alt="activate" width={400} height={80} className="w-full h-[70%]" priority />
+                    <div className="text-2xl">
+                        Discuss integration, support, and customization &gt;&gt; Compare VoiceFirst AI to other voice platforms &gt;&gt; Discuss pricing and timeline
+                    </div>
                 </div>
             </div>
             <div className="flex grow items-center justify-center">
